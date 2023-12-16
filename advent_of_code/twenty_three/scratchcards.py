da96315756a0a758c6a5753e0298612d071d57fd
Spaander <wouter.spaander@ing.com>
2023-12-16 21:11:29 +0100

    day 4 done

--- advent_of_code/twenty_three/scratchcards.py
+++ advent_of_code/twenty_three/scratchcards.py
@@ -5,6 +5,7 @@ class PileOfCards:
     def __init__(self):
         self.sum_of_cards_score = int()
         self.cards = list()
+        self.total_amount_of_cards = int()
 
     def add_card(self, card_input):
         card_number, cleaned_input = card_input.split(":")
@@ -15,17 +16,22 @@ class PileOfCards:
         self.sum_of_cards_score = sum([card.score for card in self.cards])
 
     def duplicate_cards(self):
-        for card in self.cards:
+        for i_card in self.cards:
             # add cards to list so they get counted extra?
-            pass
-            # card.matches
+            if i_card.matches:
+                for match in range(1, i_card.matches+1):
+                    card_to_duplicate = i_card.card_number + match
+                    [card.add_copy(i_card.copies) for card in self.cards if card.card_number == card_to_duplicate]
+
+        self.total_amount_of_cards = sum([card.copies for card in self.cards])
 
 
 class Scratchcard:
     def __init__(self, card_number, winning_numbers, card_numbers):
-        self.card_number = card_number.replace('   ', ' ').replace('  ', ' ').split(' ')[1]
+        self.card_number = int(card_number.replace('   ', ' ').replace('  ', ' ').split(' ')[1])
         self.winning_numbers = winning_numbers.replace('  ', ' ')[1:-1].split(' ')
         self.card_numbers = card_numbers.replace('  ', ' ')[1:].split(' ')
+        self.copies = 1
         self.matches = int()
         self.score = int()
 
@@ -38,6 +44,9 @@ class Scratchcard:
         else:
             self.score = 0
 
+    def add_copy(self, amount):
+        self.copies += amount
+
 
 def main(challenge_input):
     pile = PileOfCards()
@@ -48,8 +57,7 @@ def main(challenge_input):
     pile.calculate_sum_of_cards()
     print(pile.sum_of_cards_score)
     pile.duplicate_cards()
-    pile.calculate_sum_of_cards()
-    print(pile.sum_of_cards_score)
+    print(pile.total_amount_of_cards)
 
 
 if __name__ == "__main__":
